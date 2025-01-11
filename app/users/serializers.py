31617253e23e7ed123f2eacad34d2ed98e067add
vellimir <kkazancov@gmail.com>
2025-01-11 16:37:55 +0300

    get for email to reset your password

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.conf import settings
+
 from rest_framework import serializers
+from django.contrib.auth.tokens import default_token_generator
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,3 +30,28 @@ class UserPasswordSerializer(serializers.ModelSerializer):
             "new_password",
             "confirm_password",
         )
+
+
+class PasswordResetEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, label="Email для сброса пароля")
+
+    def validate_email(self, value):
+        try:
+            user = User.objects.get(email=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Пользователь с этим email не найден.")
+        return value
+
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        new_password = attrs.get('new_password')
+        confirm_password = attrs.get('confirm_password')
+
+        if new_password != confirm_password:
+            raise serializers.ValidationError("Пароли не совпадают.")
+        return attrs
--- app/users/views.py
+++ app/users/views.py
@@ -1,12 +1,26 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.conf import settings
+
 from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 
+from drf_yasg.utils import swagger_auto_schema
+
 from app.organizations.serializers import OrganizationUsersSerializer
 from app.organizations.models import Organization
+from settings.env_config import CONFIG__PROJECT_DOMAIN_NAME
 
-from .serializers import UserSerializer, UserPasswordSerializer
+from .serializers import UserSerializer, UserPasswordSerializer, PasswordResetEmailSerializer, \
+    PasswordResetConfirmSerializer
+
+from utils.exceptions import EXCEPTION_UNAUTHORIZED, EXCEPTION_USER_PASSWORD
 
 
 class CurrentUserView(generics.RetrieveAPIView):
@@ -49,15 +63,15 @@ class ChangePasswordView(generics.UpdateAPIView):
         new_password = serializer.validated_data['new_password']
 
         if not user.check_password(old_password):
-            return Response(
-                {"error": "Неверный текущий пароль, или новый пароль не совпадает"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            return EXCEPTION_USER_PASSWORD
 
         user.set_password(new_password)
         user.save()
 
-        return Response({"message": "Пароль успешно изменен."}, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "Пароль успешно изменен."},
+            status=status.HTTP_200_OK
+        )
 
 
 class UserDeleteAPIView(APIView):
@@ -66,11 +80,85 @@ class UserDeleteAPIView(APIView):
         if user.is_authenticated:
             user.delete()
             return Response(
-                {"message": "User deleted successfully"},
+                {"message": "Пользователь удалён"},
                 status=status.HTTP_204_NO_CONTENT
             )
         else:
+            return EXCEPTION_UNAUTHORIZED
+
+
+class PasswordResetRequestAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(request_body=PasswordResetEmailSerializer)
+    def post(self, request):
+        serializer = PasswordResetEmailSerializer(data=request.data)
+
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                return Response(
+                    {"error": "Пользователь с таким email не найден."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            self.send_reset_email(user)
+
             return Response(
-                {"error": "User not authenticated"},
-                status=status.HTTP_401_UNAUTHORIZED
+                {"message": "Письмо с инструкциями по сбросу пароля отправлено."},
+                status=status.HTTP_200_OK
             )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def send_reset_email(self, user):
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(str(user.pk).encode())
+
+        # TODO: изменить на CONFIG__PROJECT_DOMAIN_NAME
+        reset_url = f"http://127.0.0.1:8000/api/user/password-reset-confirm/{uid}/{token}/"
+
+        subject = "Восстановление пароля"
+        message = render_to_string(
+            'password_reset_email.html',
+            {'reset_url': reset_url, 'user': user}
+        )
+
+        send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email])
+
+
+class PasswordResetConfirmAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            return Response(
+                {"error": "Invalid token or user."},
+                status=400
+            )
+
+        if not default_token_generator.check_token(user, token):
+            return Response(
+                {"error": "Invalid token."},
+                status=400
+            )
+
+        serializer = PasswordResetConfirmSerializer(data=request.data)
+        if serializer.is_valid():
+            user.set_password(serializer.validated_data['new_password'])
+            user.save()
+            return Response(
+                {"message": "Пароль успешно изменен."},
+                status=200
+            )
+
+        return Response(
+            serializer.errors,
+            status=400
+        )
